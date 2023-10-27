9b376ef0676b3b02292100cd20027708d879859a
Artem Soloviev <asoloviev23@msu.idserve.net>
2023-10-27 09:41:43 -0400

    artem chapter 3 updated

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,7 +6,28 @@
 import Foundation
 
 func chapterThree() {
-    // Your portion of the story goes here
-    let name: String = "Artem"
-    print("This is \(name)'s chapter")
+    
+    let name: String = "Trav"
+    var age: Int = 25
+    var location: String = "woods"
+    let job: String = "Developer"
+
+    
+    print("Traveling \(name) at \(age) years old has been wanting to find himself a remote job as a \(job), so he could live in his camper van and be able to enjoy his life on the road and from the \(location)")
+     
+    print("Traveling Trav at 25 years old has been wanting to find himself a remote job as a developer, so he could live in his camper van and be able to enjoy his life on the road and from the place of his liking.")
+        
+    print("One day, Trav hit the road in his camper.")
+        
+    print("While Trav was driving from state to state, he continued to apply for jobs online in hopes that he might find something.")
+        
+    print("Trav was able to land a job interview via Zoom, his interview went great, and he landed his first IT job that he was able to work remotly while living in his camper on the coast of California.")
+        
+    print("He got tired of living on the coast of California, and decided to drive to Lake Tahoe so he could enjoy the mountain life.")
+        
+    print("Trav began hiking, finshing in Lake Tahoe, and snowboarding in the winter.")
+        
+    print("After spending one winter in the mountains of Lake Tahoe, he decided to relocate to the Death Valley Desert and enjoy the heat as well as peace to himself from most animals and people, since most do not visit the hot desert.")
+        
+    print("As Trav had more time to himself, he was able accomplish more work and learn much more about himself, as well as the are of being a developer.")
 }

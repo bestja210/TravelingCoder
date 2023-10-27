176208e9849e3643e4e9404ceca7e8584843bdf6
Chanel Calvin <ccalvin23@msu.idserve.net>
2023-10-27 10:31:14 -0400

    Chanel Chapter 4 Update

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -1,12 +1,38 @@
-//
-//  Chapter4.swift
-//  CLIStoryTemplate
-//
 
-import Foundation
-
-func chapterFour() {
-    // Your portion of the story goes here
-    var name: String = "Chanel"
-    print("This is \(name)'s chapter")
+class ChanelsStory {
+    static let standard: ChanelsStory = ChanelsStory(name: "Traveler", career: "Freelancer", location: "beach", hobby: "coding")
+    var name: String
+    var career: String
+    var location: String
+    var hobby: String
+    
+    init(name: String, career: String, location: String, hobby: String) {
+        self.name = name
+        self.career = career
+        self.location = location
+        self.hobby = hobby
+    }
 }
+
+let chapterNumber: Int = 4
+
+func chapterFour(){
+    
+    if ChanelsStory.standard.hobby == "coding" {
+        print("the character loves coding on the beach")
+    } else if ChanelsStory.standard.hobby == "cooking" {
+        print("the character finds a cocunt and eats it")
+    }
+    
+    print("Chapter 4")
+    print("Meet Traveler")
+    
+    
+    if ChanelsStory.standard.location == "beach" {
+        print (" the loves being on the beach watching the sunset")
+    } else if ChanelsStory.standard.location == "city" {
+        print ("the character dislikes places thats not vacation like")
+    }
+    
+}
+

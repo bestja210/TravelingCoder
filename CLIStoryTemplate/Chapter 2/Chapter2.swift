5890b3cc4709b2c4abaad5206bde0e7225820631
Kyra Gibbs <kgibbs23msu.idserve.net>
2023-11-08 08:24:58 -0500

    final

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -5,77 +5,125 @@
 
 import Foundation
 
+
+struct Person{
+    let name: String
+    let gender: Gender
+    let occuption: String
+    var location: String
+}
+
+enum Gender{
+    case female
+    case male
+    case nonbinary
+    
+    func stringify() -> String {
+        switch self{
+        case .female:
+            return "female"
+        case .male:
+            return "male"
+        case .nonbinary:
+            return "nonbinary"
+        }
+    }
+}
+let sleepTime: UInt32 = 3
+
 func chapterTwo() {
-    // Var and constant declarations
-    // var variableName: dataType = value
-    // What is the type of a variable? -> What is the variables data type aka the thing that comes after the ':'
-    let name:String = "Kyra"
-    let mainCharacterName: String = "Trav"
-    var partner: String = "Kayla"
-    var kaylaEnergy: [String] = ["Kind", "Mean", "Dismissive"]
-    var TravEnergy: String = "welcomed"
-    var kaylasCurrentEnergy: String = ""
+    print("This \("Kyra")'s chapter")
+    
+    
+    let Trav: Person = Person(name: "Trav", gender: .male, occuption: "Remote Developer", location: "Idaho")
+    
+    // arrays of answer choices
+    let answerChoices = ["kind", "mean", "dismissive"]
+    
     var userChoiceOne: String = ""
     
-    
-    print("This \(mainCharacterName)'s chapter")
-    
-    
-    
-    print("Trav stumbles upon a RV covered in bright colors, with flowers, and peace symbols everywhere.")
-    if TravEnergy == "welcomed" {
-        print("Trav will enter RV.")
-    } else if TravEnergy == "reluctant" {
-        print ("he will leave")
-    }
-    sleep(2)
-    
-    print("Trav knocks on the door and ask Kayla does she have an outlet he can use.")
-                                 //choose Kayla's mood
-    sleep(2)
-    
-    
-    print("They began to talk about how Trav ended up in a desert part of Idaho with limited battery life.")
-    sleep(2)
-   
-    print("Kayla encourages Trav to embrace the moment, feel everything, and enjoy his trip.")
-    print("Kayla has three energy states she jumps between: Kind, Mean, and Dismissive.")
-    print("Kayla is trying to decide what to do based on her mood. Please enter 1 for Kind, 2 for Mean, or 3 for Dismissive")
-    
+    print("How are you feeling today? Please enter one of the following options: \(answerChoices)")
     userChoiceOne = readLine() ?? ""
     
-    // switch (name of variable related to the value we want to compare)
-    if userChoiceOne == "1" || userChoiceOne == "2" || userChoiceOne == "3" {
-        kaylasCurrentEnergy = kaylaEnergy[Int(userChoiceOne)! - 1]
+    if answerChoices.contains(userChoiceOne.lowercased()) {
+        print("Trav's journey will unfold based upon how you feel.")
     } else {
-        print("Improper User Input. Please enter either 1, 2, or 3.")
+        print("The story can not begin until you chose from the options above.")
     }
-
-    if kaylasCurrentEnergy == "Kind" {
-        print("They will mediate!")
-    } else if kaylasCurrentEnergy == "Mean" {
-        print("they will argue")
-    } else if kaylasCurrentEnergy == "Dismissive" {
-        print("")
+    
+    switch userChoiceOne.lowercased() {
+    case "kind":
+        print ("Trav stumbles upon an RV covered in bright colors, with flowers, and peace symbols everywhere.")
+        sleep(sleepTime)
+        print ("Trav knocks on the door and Kayla answers warmly.")
+        sleep(sleepTime)
+        print ("Trav enters the RV and asks Kayla for an outlet to charge his laptop.")
+        sleep(sleepTime)
+        print("They began to talk about how Trav ended up in a desert in Idaho.")
+        sleep(sleepTime)
+        print("Kayla encourage Trav to embrace the moment, feel everything, and enjoy his trip!")
+        sleep(sleepTime)
+        print("After some mediation, and sound thearapy, Trav feels more relaxed, and calm.")
+        sleep(sleepTime)
+        print("Trav even finds time to teach Kayla how to code and how coding cand help share her experience in Idaho in the desert.")
+        sleep(sleepTime)
+        print("They laugh, learn, and explore while waiting for Trav's laptop to charge.")
+        sleep(sleepTime)
+        print("Before heading out, he thanks Kayla for letting him use her power.")
+        sleep(sleepTime)
+        print("Kayla thanks Trav for showing her how to code and trust others more.")
+        sleep(sleepTime)
+        print("Trav heads to his next adventure.")
+        
+    case "mean":
+        //print(version two of story)
+        print("Trav stumbles upon an RV covered in rust, sheer metal, with dust everywhere.")
+        sleep(sleepTime)
+        print("Trav knocks on the door and Kayla answers abruptly.")
+        sleep(sleepTime)
+        print("Trav steps back from the RV and asks Kayla for an outlet to charge his laptop.")
+        sleep(sleepTime)
+        print("Kayla rudely asks how did he find her trailer and if he was lost.")
+        sleep(sleepTime)
+        print("Kayla then slams the RV door shut!")
+        sleep(sleepTime)
+        print("After an awkard silence, Trav knocks again. Kayla slowly opens the RV door.")
+        sleep(sleepTime)
+        print("Trav asks Kayla to use an outlet. She responds by telling him to stop trespassing and go away!")
+        sleep(sleepTime)
+        print("Trav keeps walking away with his battery dying faster by the mile.")
+        sleep(sleepTime)
+        print("He hopes his next stop is more welcoming and somewhere with an outlet so he can code for a few to clear his head.")
+        sleep(sleepTime)
+        print("Trav heads to his next adventure.")
+        
+    case "dismissive":
+        // print(version three of story)
+        print("Trav stumbles upon an RV covered in dust, with fading flowers everywhere, and a lady in the window washing dishes.")
+        sleep(sleepTime)
+        print("Trav knocks on the door and Kayla answers shyly.")
+        sleep(sleepTime)
+        print("Trav asks to enter the RV. Then, asks Kayla for an outlet to charge his laptop.")
+        sleep(sleepTime)
+        print("They began to talk about how Trav ended up in a desert in Idaho and why Kayla is so reserved.")
+        sleep(sleepTime)
+        print("Kayla shares how she rarely recives visitors and that she was often alone.")
+        sleep(sleepTime)
+        print("Travs begins to speak about his journey and how he ended up at her RV.")
+        sleep(sleepTime)
+        print("They laugh and smile, and Trav even finds time to teach Kayla how to code.")
+        sleep(sleepTime)
+        print("Kayla then opens up more and shows Trav a map of the closest destination that has food, water, and power for Trav to charge all his products.")
+        sleep(sleepTime)
+        print("Before heading out, he thanks Kayla for letting him use her power.")
+        sleep(sleepTime)
+        print("Kayla thanks Trav for showing her how to code and trust others more.")
+        sleep(sleepTime)
+        print("Trav heads to his next adventure.")
+        
+    default:
+        print("Please select from the options listed: kind, dissmissive, or mean.")
     }
-    sleep(2)
-                
-    print("After meditation and listening to the sounds around him, Trav feels more relaxed.")
-    sleep(2)
-    
-    print("Trav even finds time to show Kayla to code, who is completing against technology.")
-    sleep(2)
-    
-    print("They laugh, learn, and teach other while Trav waits to for his battery to charge.")
-    sleep(2)
-    
-    print("While saying goodbye, Trav thanks Kayla for her time and power for his laptop.")
-    sleep(2)
-    
-    print("Trav heads to his next adventure.")
-    sleep(2)
-    
-    
-    
     
 }
+
